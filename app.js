e39a95fe208eb6b9690a101573ca014eb4acb98c
giftafful <affulgifty@gmail.com>
2022-01-08 11:40:40 +0000

    port changed in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const todoController = require('./controllers/todoController');
+const PORT = process.env.PORT || 3000;
 
 //route api
 app.use(express.json());
@@ -20,7 +21,7 @@ app.get('/todos/:todoId',todoController.getTodoById);
 
 
 //listener
-app.listen(3000,function(){
+app.listen(PORT,function(){
     console.log('Server has started to run');
     mongoose.connect(process.env.LOCAL_DB)
     .then(function(){
